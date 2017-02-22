c1c737d1115b3a0e66d64166c5e9129ce4706aa2
blindacai <linda_cai2014@hotmail.com>
2017-02-21 19:00:33 -0800

    add game start back to the move list

--- src/Game.js
+++ src/Game.js
@@ -45,17 +45,21 @@ class Board extends Component {
 }
 
 //////// move
-class Move extends Component{
-
-  render(){
-    const posX = this.props.move.posX;
-    const posY = this.props.move.posY;
-    const index = this.props.step;
-
-    return (
-      <li onClick={() => this.props.onClick(index)}>Move #{index + 1} at ({posX}, {posY})</li>
-    );
+function Move(props) {
+  const index = props.step;
+  let desc;
+  if(index === 0){
+    desc = props.move;
   }
+  else{
+    const posX = props.move.posX;
+    const posY = props.move.posY;
+    desc = "Move #" + index + " at (" + posX + ", " + posY +  ")";
+  }
+
+  return (
+    <li onClick={() => props.onClick(index)}>{desc}</li>
+  );
 }
 
 
@@ -78,10 +82,10 @@ class Moves extends Component{
 
   render(){
     const moves = this.props.moves.map((move, index) => {
-      return <div key={index} className={this.state.selectedItem[index]}>
+      return (<div key={index} className={this.state.selectedItem[index]}>
                 <Move move={move} step={index} onClick={(index) => {this.handleClick(index);
                                                                     this.props.onClick(index)}} />
-             </div>
+              </div>);
     }); 
 
     return (
@@ -89,7 +93,6 @@ class Moves extends Component{
     );
   }
 }
-
 
 
 //////// Game
@@ -101,7 +104,7 @@ class Game extends Component {
         squares: Array(9).fill(null)
       }],
 
-      clickAt: [],
+      clickAt: ["Game Start"],
       stepNumber: 0,
       xIsNext: true  
     };
@@ -112,7 +115,7 @@ class Game extends Component {
     const current = history[this.state.stepNumber];
     const squares = current.squares.slice();     // slice: copy the squares array instead of mutating the existing one
 
-    const clickAt = this.state.clickAt.slice(0, this.state.stepNumber);
+    const clickAt = this.state.clickAt.slice(0, this.state.stepNumber + 1);
     const index = (i-1)*3 + (j-1);
     
 
@@ -135,7 +138,7 @@ class Game extends Component {
 
   jumpTo(step){
     this.setState({
-      stepNumber: step + 1,
+      stepNumber: step,
       xIsNext: (step % 2)? false : true 
     })
   }
@@ -152,17 +155,6 @@ class Game extends Component {
     else{
       status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
     }
-
-    /*const moves = history.map((step, move) => {
-      const desc = move? 
-        'Move #' + move + ' at ' + '(' + clickAt[move - 1].posX + ',' + clickAt[move - 1].posY + ')':
-        'Game Start';
-        return (
-          <li key={move}>
-            <a href='#' onClick={() => this.jumpTo(move)}>{desc}</a>
-          </li>  
-        );
-    });*/
 
     return (
       <div className="game">  
